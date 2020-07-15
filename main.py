f04d976f2d7840369e810fb3f242aa481c015052
prateekj <psjukalkar30@gmail.com>
2020-07-14 22:13:57 -0400

    configured Selenium + scraped queried ticker price

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 from flask import Flask
-
-url = "https://finance.yahoo.com/quote/NFLX?p=NFLX&.tsrc=fin-srch"
 
 
 app = Flask(__name__)
 
 
-@app.route('/get_stock', methods=['GET'])
-def getStock():
-    response = requests.get(url)
+@app.route('/get_stock/<ticker>', methods=['GET'])
+def getStock(ticker):
+    driver = webdriver.Chrome()
+    driver.get("https://finance.yahoo.com/")
+    driver.find_element_by_id("yfin-usr-qry").send_keys(ticker)
+    driver.find_element_by_id("header-desktop-search-button").click()
+    response = requests.get(driver.current_url)
     soup = BeautifulSoup(response.text, 'html.parser')
     price = soup.find_all('div', {'class': 'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
+    driver.quit()
     return price
 
 
