f7d29946eceb8e410e14cc3b5731910338f9e0dc
pszdev30 <pszjukalkar30@gmail.com>
2020-08-15 22:06:43 -0400

    more express config stuff..

--- market-watchlists-app/server.js
+++ market-watchlists-app/server.js
@@ -1,17 +1,8 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-
-
-// Serve static files....
-app.use(express.static(__dirname + '/dist/market-watchlists-app'));
-
-
-// Send all requests to index.html
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/market-watchlists-app/index.html'));
-});
-
-
-// default Heroku PORT
-app.listen(process.env.PORT || 3000);
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
